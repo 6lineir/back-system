9aeea5c3034e4adce113c3b430dd79bfdfb82d5e
6lineir <Ali.baghaeiheravi77@gmail.com>
2021-02-12 16:05:30 +0330

    add Wallet Fileds

--- Wallet/models.py
+++ Wallet/models.py
@@ -1,5 +1,14 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
 class Wallet(models.Model):
-    pass
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    BTC_Address = models.CharField(max_length=200, blank=True)
+    BTC_Amount = models.CharField(max_length=200, blank=True)
+    LTC_Address = models.CharField(max_length=200, blank=True)
+    LTC_Amount = models.CharField(max_length=200, blank=True)
+    Doge_Address = models.CharField(max_length=200, blank=True)
+    Doge_amount = models.CharField(max_length=200, blank=True)
+    def __str__(self):
+        return self.user
